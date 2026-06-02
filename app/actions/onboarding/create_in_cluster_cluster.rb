ba498ce765551b54705abc6087cde2c2300efbd9
Chris <chris@example.com>
2026-06-02 14:59:04 +0100

    add specs for onboarding actions

--- app/actions/onboarding/create_in_cluster_cluster.rb
+++ app/actions/onboarding/create_in_cluster_cluster.rb
@@ -1,12 +1,11 @@
 class Onboarding::CreateInClusterCluster
   extend LightService::Action
   expects :account
-  promises :cluster
 
   executed do |context|
     next context unless K8::Connection.in_cluster?
 
-    context.cluster = Cluster.create!(
+    context[:cluster] = Cluster.create!(
       name: "in-cluster",
       account: context.account,
       options: { "in_cluster" => true },
