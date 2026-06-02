1c820bbfd2bdb19957751341ec53bcaa8760be23
Chris <chris@example.com>
2026-06-02 10:41:13 +0100

    added onboarding to cluster mode

--- config/routes.rb
+++ config/routes.rb
@@ -251,7 +251,7 @@ Rails.application.routes.draw do
   get "/model-context-protocol", to: "static#mcp_tools"
   get "/self-hosted", to: "static#self_hosted"
   # Public marketing homepage
-  if Rails.application.config.local_mode
+  if Rails.application.config.local_mode || Rails.application.config.cluster_mode
     namespace :local do
       resources :authentication do
         member do
